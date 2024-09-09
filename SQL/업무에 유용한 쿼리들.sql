2732dcd1d0774b6545ec6dda310aa136de22981b
Lee Min Kyung <minkyunglee1012@gmail.com>
2024-09-10 08:48:16 +0900

    업무에 유용한 쿼리들.sql

--- SQL/업무에 유용한 쿼리들.sql
+++ SQL/업무에 유용한 쿼리들.sql
@@ -1,13 +1,24 @@
--- 내가 보려고 만든 업무에 유용한 쿼리들 -- 
+-- 내가 보려고 만든 업무에 유용한, 자주 사용하는 쿼리들 -- 
 
---테이블 컬럼 개수 확인 쿼리
+
+-- 1. 보고싶은 테이블 찾기
+SELECT * 
+FROM TABLE_COLUMNS           -- VIEW도 마찬가지로 하면 된다
+WHERE 1=1 
+AND SCHEMA_NAME = '스키마명'
+AND TABLE_NAME LIKE '%CQ_%'
+ORDER BY POSITION 
+
+  
+-- 2. 테이블 컬럼 개수 확인 쿼리
 SELECT count(*)
 FROM table_columns
-WHERE table_name = '테이블 명'
+WHERE 1=1 
+AND table_name = '테이블 명'
 AND schema_name = '스키마 명'
 
   
--- 테이블 그룹별 코드 카운트 쿼리
+-- 3. 테이블 그룹별 코드 카운트 쿼리
 SELECT 
     'COL1' AS COLUMN_NAME, 
     COL1 AS VALUE, 
@@ -32,7 +43,7 @@ GROUP BY COL3
 ORDER BY COLUMN_NAME, VALUE 
 
   
--- 중복 개수 확인
+-- 4. 중복 개수 확인
 SELECT COUNT(1), COUNT(distinct COL1)
 FROM 테이블명
 
@@ -42,6 +53,7 @@ GROUP BY COL1
 HAVING count(COL1) > 1
 
 
+-- 5. 데이터 및 테이블 삭제
 -- 특정 행 데이터 삭제
 DELETE FROM 테이블명 WHERE a = '_';
 -- 모든 데이터 삭제
@@ -50,16 +62,7 @@ TRUNCATE TABLE 스키마명.테이블명
 DROP TABLE 테이블명
 
   
--- 보고싶은 테이블 찾기
-SELECT * 
-FROM TABLE_COLUMNS           -- VIEW도 마찬가지로 하면 된다
-WHERE 1=1 
-AND SCHEMA_NAME = '스키마명'
-AND TABLE_NAME LIKE '%CQ_%'
-ORDER BY POSITION 
-
-  
--- 중복 있을 때 컬럼명은 그대로 새로운 테이블 만들어서(1) 중복 없애고 데이터 값 넣기(2)
+-- 6. 중복 있을 때 컬럼명은 그대로 새로운 테이블 만들어서(1) 중복 없애고 데이터 값 넣기(2)
 CREATE COLUMN TABLE 스키마명.새로운테이블명
 LIKE 스키마명.기존테이블명;
 
